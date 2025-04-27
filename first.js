f256c4f378ebda347ba79250b9eb632135f60119
sohanchowdhury <sohanchowdhury1212@gmail.com>
2025-04-28 00:22:41 +0600

    mobile show

--- first.js
+++ first.js
@@ -1,5 +1,17 @@
-sohan = "hello world"
+let score = 75;
+let grade;
+if ( score >= 90 && score >= 100 ){
+    grade = "A";
+}else if ( score >= 70 && score <= 89 ){
+    grade = "B";
+}else if ( score >= 60 && score <= 69 ){
+    grade = "C";
+}else if ( score >= 50 && score <= 59 ){
+    grade = "D";
+}else if ( score >= 0 && score <= 49 ){
+    grade = "F";
+}
+console.log(grade)
 
-console.log(sohan);
 
 
